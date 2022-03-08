b67b6318294e3fa1e69881815bd8099636662659
Angryman18 <imsmahanta@gmail.com>
2022-03-08 10:23:07 +0530

    basic setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,12 @@ import ReactImage from "../images/react-pic.png";
 
 const Home = () => {
   return (
-    <div className='h-screen'>
-      <div className='flex justify-center items-center h-3/4'>
-        <span className='text-2xl my-4'>Congrats! You have successfully installed React customely configured with Webpack, Babel and Workbox for PWA </span>
-        {/* <img className="w-96" src={ReactImage} alt="" /> */}
+    <div className=''>
+      <div className='flex justify-center flex-col items-center h-3/4'>
+        <span className='text-2xl my-4 text-center z-10 text-white'>
+          React with Webpack and Babel Setup is Complete
+        </span>
+        <img className='w-full object-center object-cover h-full absolute top-0 left-0 right-0 bottom-0' src={ReactImage} alt='' />
       </div>
     </div>
   );
